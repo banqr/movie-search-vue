2d7e0f2f0a1ba9718e04ef035be6b302b1c0ce3d
Radovan Draskovic <radovandraskovic@gmail.com>
2019-04-14 00:20:13 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,50 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h3>Movie search</h3>
+    <form @submit.prevent="hello">
+      <div class="row">
+        <div class="six columns">
+          <label for="movie">Your movie</label>
+          <input v-model="movie" class="u-full-width" type="text" id="movie">
+        </div>
+      </div>
+      <input class="button-primary" type="submit" value="Submit">
+    </form>
+    <img v-bind:src="imgage" v-if="seen">
   </div>
 </template>
 
+<script>
+const giphy_url_1 = 'https://api.giphy.com/v1/gifs/search?api_key=tRoV6ISux00Pr8859b5RJF0p0BSACosj&q='
+const giphy_url_2 = '&limit=25&offset=0&rating=G&lang=en'
+const axios = require('axios')
+export default {
+  name: 'app',
+  data() {
+    return {
+      seen: false,
+      movie: '',
+      gifs: [],
+      imgage: ''
+    }
+  },
+  methods: {
+    hello: function(){
+      axios.get(`${giphy_url_1}${this.movie}${giphy_url_2}`)
+        .then(res => this.gifs = res.data)
+        .then(dejta => {
+          this.seen = true
+          this.imgage = dejta.data[0].images.downsized.url
+          this.movie = ''    
+          console.log(dejta.data[0].images.downsized.url)
+          })
+      //console.log(`${giphy_url_1}${this.movie}${giphy_url_2}`)
+      
+      
+    }
+  },
+}
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
